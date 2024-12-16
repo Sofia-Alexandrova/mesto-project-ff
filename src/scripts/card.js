5ba12c7804ad559ad0887d4f974f9eabd7658dab
Sofia Alexandrova <alxxxa65@yandex.ru>
2024-12-16 21:40:46 +0300

    Initial commit

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,25 +1,39 @@
-export function createCard(cardTemplate, card, deleteCallback, likeCallback, clickImageCallback) {
-	const cardElement = cardTemplate.cloneNode(true);
-	const cardImage = cardElement.querySelector('.card__image');
-	const cardTitle = cardElement.querySelector('.card__title');
-	const cardDeleteButton = cardElement.querySelector('.card__delete-button');
-	const cardLikeButton = cardElement.querySelector('.card__like-button');
+export function createCard(
+  cardTemplate,
+  card,
+  deleteCallback,
+  likeCallback,
+  clickImageCallback
+) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+  const cardLikeButton = cardElement.querySelector(".card__like-button");
 
-	cardImage.src = card.link;
-	cardImage.alt = card.alt;
-	cardTitle.textContent = card.name;
+  cardImage.src = card.link;
+  cardImage.alt = card.alt;
+  cardTitle.textContent = card.name;
 
-	cardDeleteButton.addEventListener('click', () => deleteCallback(cardElement, card));
-	cardLikeButton.addEventListener('click', () => likeCallback(cardElement, card));
-	cardImage.addEventListener('click', () => clickImageCallback(cardElement, card));
+  cardDeleteButton.addEventListener("click", () =>
+    deleteCallback(cardElement, card)
+  );
+  cardLikeButton.addEventListener("click", () =>
+    likeCallback(cardElement, card)
+  );
+  cardImage.addEventListener("click", () =>
+    clickImageCallback(cardElement, card)
+  );
 
-	return cardElement;
+  return cardElement;
 }
 
 export function deleteCard(cardElement, _) {
-	cardElement.remove();
+  cardElement.remove();
 }
 
 export function toggleLike(cardElement, _) {
-	cardElement.querySelector('.card__like-button').classList.toggle('card__like-button_is-active');
+  cardElement
+    .querySelector(".card__like-button")
+    .classList.toggle("card__like-button_is-active");
 }
